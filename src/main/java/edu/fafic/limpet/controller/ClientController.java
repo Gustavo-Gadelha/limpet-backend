4f1b1d3ce526f4f49554f9d775b320eb52e008c9
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-04-30 10:53:29 -0300

    chore: Remoção de imports não utilizados

--- src/main/java/edu/fafic/limpet/controller/ClientController.java
+++ src/main/java/edu/fafic/limpet/controller/ClientController.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
